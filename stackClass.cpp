f4784289fe065abca07dd035d75fb610b1faf32a
Sarah-56 <59021799+Sarah-56@users.noreply.github.com>
2022-08-20 16:40:31 +0200

    Add files via upload

--- stackClass.cpp
+++ stackClass.cpp
@@ -10,20 +10,16 @@ struct node
 class stack
 {
     node *head;
-    node *tail;
 public:
     stack(){
         head = NULL;
-        tail = NULL;
     }
     void push(int data){
         node *new_node = new node;
         new_node->data = data;
         new_node->next = head;
         head = new_node;
-        if(tail == NULL){
-            tail = new_node;
-        }
+       
     }
     void pop(){
         node *temp = head;
